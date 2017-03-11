3a670ad4524fa2316409b2c3a2f1ef0ff037faab
carrotRakko <kunimaru.k.u@gmail.com>
2017-03-11 15:45:35 +0900

    Add files via upload

---
+++ pieces.js
@@ -0,0 +1,723 @@
+var WIDTH = 16;
+var HEIGHT = 16;
+
+var WHITE = {
+  P :  0, CP:  1, K :  2, PR:  3, EL:  4, MC:  5,
+  N :  6, CM:  7, BL:  8, LN:  9, BF: 10, AN: 11,
+  B : 12, R : 13, Q : 14, BW: 15, CN: 16, ST: 17,
+  SH: 18, GR: 19, RH: 20, CD: 21, MS: 22, UN: 23, KG: -1, MK: -2
+};
+var INVERTWHITE = _.invert(WHITE);
+
+
+var BLACK = {
+  P : 24, CP: 25, K : 26, PR: 27, EL: 28, MC: 29,
+  N : 30, CM: 31, BL: 32, LN: 33, BF: 34, AN: 35,
+  B : 36, R : 37, Q : 38, BW: 39, CN: 40, ST: 41,
+  SH: 42, GR: 43, RH: 44, CD: 45, MS: 46, UN: 47, KG: 48, MK: 49
+};
+var INVERTBLACK = _.invert(BLACK);
+
+// var DEFAULT = [
+//   [35, 25, 28, 24, null, null, null, null, null, null, null, null,  0,  4,  1, 11],
+//   [31, 25, 29, 24, null, null, null, null, null, null, null, null,  0,  5,  1,  7],
+//   [39, 25, 37, 24, null, null, null, null, null, null, null, null,  0, 13,  1, 15],
+//   [32, 25, 30, 24, null, null, null, null, null, null, null, null,  0,  6,  1,  8],
+//   [40, 25, 36, 24, null, null, null, null, null, null, null, null,  0, 12,  1, 16],
+//   [45, 25, 42, 24, null, null, null, null, null, null, null, null,  0, 18,  1, 21],
+//   [46, 25, 27, 24, null, null, null, null, null, null, null, null,  0,  3,  1, 22],
+//   [47, 43, 26, 24, null, null, null, null, null, null, null, null,  0,  2, 19, 23],
+//   [41, 33, 38, 24, null, null, null, null, null, null, null, null,  0, 14,  9, 17],
+//   [44, 25, 27, 24, null, null, null, null, null, null, null, null,  0,  3,  1, 20],
+//   [34, 25, 42, 24, null, null, null, null, null, null, null, null,  0, 18,  1, 10],
+//   [40, 25, 36, 24, null, null, null, null, null, null, null, null,  0, 12,  1, 16],
+//   [32, 25, 30, 24, null, null, null, null, null, null, null, null,  0,  6,  1,  8],
+//   [39, 25, 37, 24, null, null, null, null, null, null, null, null,  0, 13,  1, 15],
+//   [31, 25, 29, 24, null, null, null, null, null, null, null, null,  0,  5,  1,  7],
+//   [35, 25, 28, 24, null, null, null, null, null, null, null, null,  0,  4,  1, 11],
+// ];
+var DEFAULT = [
+  [35, 25, 28, 24, null, null, null, null, null, null, null, null,  0,  4,  1, 11],
+  [31, 25, 29, 24, null, null, null, null, null, null, null, null,  0,  5,  1,  7],
+  [39, 25, 37, 24, null, null, null, null, null,   49, null, null,  0, 13,  1, 15],
+  [32, 25, 30, 24, null, null, null, null, null, null, null, null,  0,  6,  1,  8],
+  [40, 25, 36, 24, null, null,   48, null, null, null, null, null,  0, 12,  1, 16],
+  [45, 25, 42, 24, null, null, null, null, null, null, null, null,  0, 18,  1, 21],
+  [46, 25, 27, 24, null, null, null, null, null, null, null, null,  0,  3,  1, 22],
+  [47, 43, 26, 24, null, null, null, null, null, null, null, null,  0,  2, 19, 23],
+  [41, 33, 38, 24, null, null, null, null, null, null, null, null,  0, 14,  9, 17],
+  [44, 25, 27, 24, null, null, null, null, null, null, null, null,  0,  3,  1, 20],
+  [34, 25, 42, 24, null, null, null, null, null, null, null, null,  0, 18,  1, 10],
+  [40, 25, 36, 24, null, null, null, null, null,   -1, null, null,  0, 12,  1, 16],
+  [32, 25, 30, 24, null, null, null, null, null, null, null, null,  0,  6,  1,  8],
+  [39, 25, 37, 24, null, null,   -2, null, null, null, null, null,  0, 13,  1, 15],
+  [31, 25, 29, 24, null, null, null, null, null, null, null, null,  0,  5,  1,  7],
+  [35, 25, 28, 24, null, null, null, null, null, null, null, null,  0,  4,  1, 11],
+];
+
+// ここから下まで MOVE
+var MOVE = {};
+
+MOVE.P = {
+  type: 'pawn'
+};
+
+MOVE.CP = {
+  type: 'corporal'
+};
+
+MOVE.K = {
+  type: 'jump',
+  value: [
+    {col: +1, row: +1},
+    {col: +1, row: -1},
+    {col: -1, row: +1},
+    {col: -1, row: -1},
+    {col: +1, row:  0},
+    {col:  0, row: +1},
+    {col: -1, row:  0},
+    {col:  0, row: -1}
+  ]
+};
+
+MOVE.PR = MOVE.K;
+
+MOVE.EL = {
+  type: 'jump',
+  value: [
+    {col: +1, row: +1},
+    {col: +1, row: -1},
+    {col: -1, row: +1},
+    {col: -1, row: -1},
+    {col: +2, row: +2},
+    {col: +2, row: -2},
+    {col: -2, row: +2},
+    {col: -2, row: -2}
+  ]
+};
+
+MOVE.MC = {
+  type: 'jump',
+  value: [
+    {col: +1, row:  0},
+    {col:  0, row: +1},
+    {col: -1, row:  0},
+    {col:  0, row: -1},
+    {col: +2, row:  0},
+    {col:  0, row: +2},
+    {col: -2, row:  0},
+    {col:  0, row: -2}
+  ]
+};
+
+MOVE.N = {
+  type: 'jump',
+  value: [
+    {col: +1, row: +2},
+    {col: +1, row: -2},
+    {col: -1, row: +2},
+    {col: -1, row: -2},
+    {col: +2, row: +1},
+    {col: +2, row: -1},
+    {col: -2, row: +1},
+    {col: -2, row: -1}
+  ]
+};
+
+MOVE.CM = {
+  type: 'jump',
+  value: [
+    {col: +1, row: +3},
+    {col: +1, row: -3},
+    {col: -1, row: +3},
+    {col: -1, row: -3},
+    {col: +3, row: +1},
+    {col: +3, row: -1},
+    {col: -3, row: +1},
+    {col: -3, row: -1}
+  ]
+};
+
+MOVE.BL = {
+  type: 'jump',
+  value: [
+    {col: +2, row: +3},
+    {col: +2, row: -3},
+    {col: -2, row: +3},
+    {col: -2, row: -3},
+    {col: +3, row: +2},
+    {col: +3, row: -2},
+    {col: -3, row: +2},
+    {col: -3, row: -2}
+  ]
+};
+
+MOVE.LN = {
+  type: 'parallel',
+  value: [
+    MOVE.N,
+    MOVE.EL,
+    MOVE.MC
+  ]
+};
+
+MOVE.BF = {
+  type: 'parallel',
+  value: [
+    MOVE.N,
+    MOVE.CM,
+    MOVE.BL
+  ]
+};
+
+MOVE.AN = {
+  type: 'jump',
+  value: [
+    {col: +2, row: +2},
+    {col: +2, row: -2},
+    {col: -2, row: +2},
+    {col: -2, row: -2},
+    {col: +3, row: +3},
+    {col: +3, row: -3},
+    {col: -3, row: +3},
+    {col: -3, row: -3},
+    {col: +2, row:  0},
+    {col:  0, row: +2},
+    {col: -2, row:  0},
+    {col:  0, row: -2},
+    {col: +3, row:  0},
+    {col:  0, row: +3},
+    {col: -3, row:  0},
+    {col:  0, row: -3}
+  ]
+}
+
+MOVE.B = {
+  type: 'run',
+  value: [
+    {col: +1, row: +1},
+    {col: +1, row: -1},
+    {col: -1, row: +1},
+    {col: -1, row: -1}
+  ]
+};
+
+MOVE.R = {
+  type: 'run',
+  value: [
+    {col: +1, row:  0},
+    {col:  0, row: +1},
+    {col: -1, row:  0},
+    {col:  0, row: -1}
+  ]
+};
+
+MOVE.Q = {
+  type: 'parallel',
+  value: [
+    MOVE.B,
+    MOVE.R
+  ]
+};
+
+MOVE.BW= {
+  type: 'bow',
+  value: [
+    {col: +1, row: +1},
+    {col: +1, row: -1},
+    {col: -1, row: +1},
+    {col: -1, row: -1}
+  ]
+};
+
+MOVE.CN = {
+  type: 'bow',
+  value: [
+    {col: +1, row:  0},
+    {col:  0, row: +1},
+    {col: -1, row:  0},
+    {col:  0, row: -1}
+  ]
+};
+
+MOVE.ST = {
+  type: 'parallel',
+  value: [
+    MOVE.BW,
+    MOVE.CN
+  ]
+};
+
+MOVE.SH = {
+  type: 'parallel',
+  value: [
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: -1}
+          ]
+        }
+      ]
+    },
+  ]
+};
+
+MOVE.GR = {
+  type: 'parallel',
+  value: [
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row:  0},
+            {col:  0, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row:  0},
+            {col:  0, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row:  0},
+            {col:  0, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row:  0},
+            {col:  0, row: -1}
+          ]
+        }
+      ]
+    },
+  ]
+};
+
+MOVE.RH = {
+  type: 'parallel',
+  value: [
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: +2}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +1, row: -2}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: +2}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -1, row: -2}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +2, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: +2, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -2, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'jump',
+          value: [
+            {col: -2, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: -1}
+          ]
+        }
+      ]
+    },
+  ]
+};
+
+MOVE.CD = {
+  type: 'parallel',
+  value: [
+    MOVE.N,
+    MOVE.B
+  ]
+};
+
+MOVE.MS = {
+  type: 'parallel',
+  value: [
+    MOVE.N,
+    MOVE.R
+  ]
+};
+
+MOVE.UN = {
+  type: 'parallel',
+  value: [
+    MOVE.N,
+    MOVE.Q
+  ]
+};
+
+MOVE.KG = {
+  type: 'parallel',
+  value: [
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: 0}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: +1},
+            {col: 0, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: 0}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: +1},
+            {col: 0, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: 0},
+            {col: -1, row: 0}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: 0, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: 0},
+            {col: -1, row: 0}
+          ]
+        }
+      ]
+    },
+  ]
+},
+
+MOVE.MK = {
+  type: 'parallel',
+  value: [
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: +1},
+            {col: +1, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: -1},
+            {col: +1, row: +1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: +1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: +1},
+            {col: -1, row: -1}
+          ]
+        }
+      ]
+    },
+    {
+      type: 'series',
+      value: [
+        {
+          type: 'run',
+          value: [
+            {col: -1, row: -1}
+          ]
+        },
+        {
+          type: 'run',
+          value: [
+            {col: +1, row: -1},
+            {col: -1, row: +1}
+          ]
+        }
+      ]
+    },
+  ]
+}
---
+++ terachess.js
@@ -0,0 +1,400 @@
+$(function(){
+  var DEBUG_MODE = true;
+
+  // 初期化
+  var board = _.cloneDeep(DEFAULT);
+  var select = null;
+  var ables = [];
+  var rendered = render(board);
+  addPiece();
+
+  // main 開始
+  $('.terachess').on('click', function(event){
+    $('.terachess').off('click', arguments.callee);
+    var index = $('.terachess_cell').index(event.target);
+    var col = Math.floor(index / 16);
+    var row = index % 16;
+    if(select === null){ // 未選択
+      if(isExist(col, row)){ // 駒が存在
+        select = {col: col, row: row};
+        addSelect();
+        makeAbles();
+        addAble();
+      }
+    } else if(select !== null){ // 選択
+      if(col === select.col && row === select.row){ // 自身
+        removeSelect();
+        select = null;
+        removeAble();
+        ables = [];
+      } else if(isAble(col, row)){ // 可能手
+        removePiece();
+        move({
+          from: {
+            col: select.col,
+            row: select.row
+          },
+          to: {
+            col: col,
+            row: row
+          }
+        });
+        addPiece();
+        removeSelect();
+        select = null;
+        removeAble();
+        ables = [];
+      }
+    }
+    $('.terachess').on('click', arguments.callee);
+  });
+  // main 終了
+
+  function render(board){
+    var ret = [];
+    var terachess = $('<div>')
+      .addClass('terachess')
+      .appendTo('body');
+    for(var col = 0; col < HEIGHT; col++){
+      ret[col] = [];
+      for(var row = 0; row < WIDTH; row++){
+        ret[col][row] = $('<div>')
+          .addClass('terachess_cell')
+          .addClass((col % 2 == 0) ^ (row % 2 == 0) ? 'black' : 'white')
+          .appendTo(terachess);
+      }
+    }
+    return ret;
+  }
+
+  function addSelect(){
+    rendered[select.col][select.row].addClass('select');
+  }
+  function removeSelect(){
+    rendered[select.col][select.row].removeClass('select');
+  }
+  function addAble(){
+    _.forEach(ables, function(able){
+      rendered[able.col][able.row].addClass('able');
+    });
+  }
+  function removeAble(){
+    _.forEach(ables, function(able){
+      rendered[able.col][able.row].removeClass('able');
+    });
+  }
+  function addPiece(){
+    for(var col = 0; col < HEIGHT; col++){
+      for(var row = 0; row < WIDTH; row++){
+        if(isWhite(col, row)){
+          rendered[col][row].addClass(INVERTWHITE[board[col][row]]);
+        } else if(isBlack(col, row)){
+          rendered[col][row].addClass('v' + INVERTBLACK[board[col][row]]);
+        }
+      }
+    }
+  }
+  function removePiece(){
+    for(var col = 0; col < HEIGHT; col++){
+      for(var row = 0; row < WIDTH; row++){
+        if(isWhite(col, row)){
+          rendered[col][row].removeClass(INVERTWHITE[board[col][row]]);
+        } else if(isBlack(col, row)){
+          rendered[col][row].removeClass('v' + INVERTBLACK[board[col][row]]);
+        }
+      }
+    }
+  }
+
+  function move(data){
+    board[data.to.col][data.to.row] = board[data.from.col][data.from.row];
+    board[data.from.col][data.from.row] = null;
+  }
+  function makeAbles(){
+    var piece;
+    var color;
+    if(isWhite(select.col, select.row)){
+      piece = INVERTWHITE[board[select.col][select.row]];
+      color = 'white';
+    } else if(isBlack(select.col, select.row)){
+      piece = INVERTBLACK[board[select.col][select.row]];
+      color = 'black';
+    }
+    var move = MOVE[piece];
+    ables = compile(move, color, {col: select.col, row: select.row});
+  }
+
+  function isOnBoard(col, row){
+    return col >= 0 && col < WIDTH && row >= 0 && row < HEIGHT;
+  }
+  function isWhite(col, row){
+    return _.includes(WHITE, board[col][row]);
+  }
+  function isBlack(col, row){
+    return _.includes(BLACK, board[col][row]);
+  }
+  function isExist(col, row){
+    return isWhite(col, row) || isBlack(col, row);
+  }
+  function isEmpty(col, row){
+    return !isWhite(col, row) && !isBlack(col, row);
+  }
+  function isAble(col, row){
+    if(DEBUG_MODE) return true;
+    return _.includes(ables, board[col][row]);
+  }
+
+  function compile(move, color, start){
+    switch(move.type){
+      case 'pawn':
+        var ret = [];
+        var col = start.col;
+        var row = start.row;
+        switch(color){
+          case 'white':
+            if(isOnBoard(col, row - 1) && isEmpty(col, row - 1)){
+              ret.push({col: col, row: row - 1});
+            }
+            if(isEmpty(col, row - 1) && isOnBoard(col, row - 2) && isEmpty(col, row - 2)){
+              ret.push({col: col, row: row - 2});
+            }
+            if(isOnBoard(col + 1, row - 1) && isEnemy(col + 1, row - 1)){
+              ret.push({col: col + 1, row: row - 1});
+            }
+            if(isOnBoard(col - 1, row - 1) && isEnemy(col - 1, row - 1)){
+              ret.push({col: col - 1, row: row - 1});
+            }
+          break;
+          case 'black':
+            if(isOnBoard(col, row + 1) && isEmpty(col, row + 1)){
+              ret.push({col: col, row: row + 1});
+            }
+            if(isEmpty(col, row + 1) && isOnBoard(col, row + 2) && isEmpty(col, row + 2)){
+              ret.push({col: col, row: row + 2});
+            }
+            if(isOnBoard(col + 1, row + 1) && isEnemy(col + 1, row + 1)){
+              ret.push({col: col + 1, row: row + 1});
+            }
+            if(isOnBoard(col - 1, row + 1) && isEnemy(col - 1, row + 1)){
+              ret.push({col: col - 1, row: row + 1});
+            }
+          break;
+        }
+        return ret;
+      break;
+      case 'corporal':
+        var ret = [];
+        var col = start.col;
+        var row = start.row;
+        switch(color){
+          case 'white':
+            if(isOnBoard(col, row - 1) && isEmpty(col, row - 1)){
+              ret.push({col: col, row: row - 1});
+            }
+            if(isEmpty(col, row - 1) && isOnBoard(col, row - 2) && isEmpty(col, row - 2)){
+              ret.push({col: col, row: row - 2});
+            }
+            if(isOnBoard(col + 1, row - 1) && (isEnemy(col + 1, row - 1) || isEmpty(col + 1, row - 1))){
+              ret.push({col: col + 1, row: row - 1});
+            }
+            if(isOnBoard(col - 1, row - 1) && (isEnemy(col - 1, row - 1) || isEmpty(col - 1, row - 1))){
+              ret.push({col: col - 1, row: row - 1});
+            }
+          break;
+          case 'black':
+            if(isOnBoard(col, row + 1) && isEmpty(col, row + 1)){
+              ret.push({col: col, row: row + 1});
+            }
+            if(isEmpty(col, row + 1) && isOnBoard(col, row + 2) && isEmpty(col, row + 2)){
+              ret.push({col: col, row: row + 2});
+            }
+            if(isOnBoard(col + 1, row + 1) && (isEnemy(col + 1, row + 1) || isEmpty(col + 1, row + 1))){
+              ret.push({col: col + 1, row: row + 1});
+            }
+            if(isOnBoard(col - 1, row + 1) && (isEnemy(col - 1, row + 1) || isEmpty(col - 1, row + 1))){
+              ret.push({col: col - 1, row: row + 1});
+            }
+          break;
+        }
+        return ret;
+      break;
+      case 'jump':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            var head = _.head(move.value);
+            var target = {col: start.col + head.col, row: start.row + head.row};
+            return _.concat(
+              isOnBoard(target.col, target.row) && (isEnemy(target.col, target.row) || isEmpty(target.col, target.row)) ? [target] : [],
+              compile({
+                type: 'jump',
+                value: _.tail(move.value)
+              }, color, start)
+            );
+          break;
+        }
+      break;
+      case 'run':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            var head = _.head(move.value);
+            var target = {col: start.col + head.col, row: start.row + head.row};
+            var run;
+            if(isOnBoard(target.col, target.row) && isAlly(target.col, target.row)){
+              run = [];
+            } else if(isOnBoard(target.col, target.row) && isEnemy(target.col, target.row)){
+              run = [target];
+            } else if(isOnBoard(target.col, target.row) && isEmpty(target.col, target.row)){
+              run = _.concat(
+                [target],
+                compile({
+                  type: 'run',
+                  value: [head]
+                }, color, target)
+              );
+            } else{
+              run = [];
+            }
+            return _.concat(
+              run,
+              compile({
+                type: 'run',
+                value: _.tail(move.value)
+              }, color, start)
+            );
+          break;
+        }
+      break;
+      case 'capture_first':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            var head = _.head(move.value);
+            var target = {col: start.col + head.col, row: start.row + head.row};
+            var capture_first;
+            if(isOnBoard(target.col, target.row) && isAlly(target.col, target.row)){
+              capture_first = [];
+            } else if(isOnBoard(target.col, target.row) && isEnemy(target.col, target.row)){
+              capture_first = [target];
+            } else if(isOnBoard(target.col, target.row) && isEmpty(target.col, target.row)){
+              capture_first = compile({
+                type: 'capture_first',
+                value: [head]
+              }, color, target)
+            } else{
+              capture_first = [];
+            }
+            return _.concat(
+              capture_first,
+              compile({
+                type: 'capture_first',
+                value: _.tail(move.value)
+              }, color, start)
+            );
+          break;
+        }
+      break;
+      case 'bow':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            var head = _.head(move.value);
+            var target = {col: start.col + head.col, row: start.row + head.row};
+            var bow;
+            if(isOnBoard(target.col, target.row) && (isAlly(target.col, target.row) || isEnemy(target.col, target.row))){
+              bow = compile({
+                type: 'capture_first',
+                value: [head]
+              }, color, target);
+            } else if(isOnBoard(target.col, target.row) && isEmpty(target.col, target.row)){
+              bow = _.concat(
+                [target],
+                compile({
+                  type: 'bow',
+                  value: [head]
+                }, color, target)
+              );
+            } else{
+              bow = [];
+            }
+            return _.concat(
+              bow,
+              compile({
+                type: 'bow',
+                value: _.tail(move.value)
+              }, color, start)
+            );
+          break;
+        }
+      break;
+      case 'parallel':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            return _.concat(
+              compile(_.head(move.value), color, start),
+              compile({
+                type: 'parallel',
+                value: _.tail(move.value)
+              }, color, start)
+            );
+          break;
+        }
+      break;
+      case 'series':
+        switch(move.value.length){
+          case 0:
+            return [];
+          break;
+          default:
+            var head = _.head(move.value);
+            var firsts = compile(head, color, start);
+            return _.concat(
+              firsts,
+              _.flatten(
+                _.map(firsts, function(first){
+                  return compile({
+                    type: 'series',
+                    value: _.tail(move.value)
+                  }, color, first);
+                })
+              )
+            );
+          break;
+        }
+      break;
+    }
+
+    function isAlly(col, row){
+      switch(color){
+        case 'white':
+          return isWhite(col, row);
+        break;
+        case 'black':
+          return isBlack(col, row);
+        break;
+      }
+    }
+    function isEnemy(col, row){
+      switch(color){
+        case 'white':
+          return isBlack(col, row);
+        break;
+        case 'black':
+          return isWhite(col, row);
+        break;
+      }
+    }
+  }
+});
