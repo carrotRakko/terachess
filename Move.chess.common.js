8d0cfb1b7a1b18f97af6c45ab1321e1cb6b31865
carrotRakko <kunimaru.k.u@gmail.com>
2017-03-12 11:05:01 +0900

    new move file

---
+++ Move.chess.common.js
@@ -0,0 +1,20 @@
+// 標準チェスの動きファイル
+// 依存ファイルはない
+(function(){
+  var Move = Move || {};
+  Move.chess = Move.chess || {};
+
+  var common = {};
+
+  common.P = {
+    type: ,
+    value:
+  };
+
+  common.K = {
+    type: ,
+    value:
+  };
+
+  Move.chess.common = common;
+)();
